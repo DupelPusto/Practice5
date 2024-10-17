96f52ca4dd0c6fca6eaa614a82ab35cb68b8c8d2
DupelPusto <borisjenya2@gmail.com>
2024-10-17 22:20:58 +0300

    Add files via upload

--- Pract5First.java
+++ Pract5First.java
@@ -1,6 +1,7 @@
 
 public class Pract5First
 {
+    //Створення констант необхідних за умовою
     public static final double a = 0.3;
     public static final double b = 0.9;
     public static final double c = 0.61;
@@ -8,12 +9,21 @@ public class Pract5First
 
     public static void main(String[] args)
     {
+        //Проведення обчислень в 3 етапи
+
+        //Знаходимо перший доданок прикладу
         double first = Math.pow(a, 2 * c) + (Math.pow(b, -c) * Math.cos(Math.toRadians(a) + Math.toRadians(b)));
+
+        //Знаходимо другий доданок прикладу
         double second = c + 1;
+
+        //Знаходимо результат прикладу
         double result = first + second;
+
+        //Виведення результату обчислення прикладу
         System.out.println("Result is: " + result);
 
-
-
+        //Завершення програми
     }
+
 }
--- Pract5Second.java
+++ Pract5Second.java
@@ -7,31 +7,41 @@ public class Pract5Second
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
+        // Введення користувачем занчення змінної x
         System.out.println("Enter x value:");
         double x = sc.nextDouble();
 
+        // Введення користувачем занчення змінної a
         System.out.println("Enter a value:");
         double a = sc.nextDouble();
 
+        // Введення користувачем занчення змінної b
         System.out.println("Enter b value:");
         double b = sc.nextDouble();
 
         double res;
 
-        if (x > -3 && x < 2)
+        if (x > -3 && x < 2) //Перевірка першої умови
         {
             res = Math.sqrt(a * x + 2);
             System.out.println("The result is: " + res);
-        } else if (x == 2){
+        }
+        else if (x == 2) // Перевірка другої умови
+        {
             res = Math.cos(Math.toRadians(2 * x));
             System.out.println("The result is: " + res);
-        } else if (x > 2) {
+        }
+        else if (x > 2) // Перевірка третьої умови
+        {
             res =  (3 * Math.pow(x, 2)) + (b * x) + 1;
             System.out.println("The result is: " + res);
-        } else {
+        }
+        else // Виведення у разі якщо жодна з умов не була виконана
+        {
             System.out.println("The number you entered is incorrect. Try again");
         }
-
+    //Завершення програми
 
     }
 }
